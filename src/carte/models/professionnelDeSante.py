68b75ab41aa29357937d509fef5b936fabf31904
root <root@cst-p-app-fd8b.localdomain>
2024-05-31 12:13:25 +0200

    Mise à jour du visuel des pages

--- src/carte/models/professionnelDeSante.py
+++ src/carte/models/professionnelDeSante.py
@@ -3,17 +3,20 @@ from django.db import models
 
 class Profession(models.Model):
     idProfession = models.AutoField(primary_key=True)
-    lib = models.CharField(max_length=120)
+    codeProfession = models.IntegerField()
+    libProfession = models.CharField(max_length=120)
 
 class Specialite(models.Model):
     idSpecialite = models.AutoField(primary_key=True)
-    lib = models.CharField(max_length=120)
+    codeSpecialite = models.IntegerField()
+    libSpecialite = models.CharField(max_length=120)
 
 class ProfessionnelDeSante(models.Model):
     idProfessionnelDeSante = models.AutoField(primary_key=True)
+    identifiantPP = models.IntegerField()
     nom = models.CharField(max_length=60)
     prenom = models.CharField(max_length=60)
     dateNaiss = models.DateField()
     dateMaj = models.DateField()
     profession = models.ForeignKey(Profession, on_delete=models.CASCADE)
-    specialite = models.ForeignKey(Specialite, on_delete=models.CASCADE)
+    specialite = models.ForeignKey(Specialite, on_delete=models.CASCADE, null = True)
